08cdb1817ad761312be19d54603c96c2023ca766
Leela <lnm29@pitt.edu>
2018-03-16 22:08:50 -0400

    Just added the buttons

--- app/src/main/java/edu/pitt/cs/cs1635/amp224/closetcase/MainActivity.java
+++ app/src/main/java/edu/pitt/cs/cs1635/amp224/closetcase/MainActivity.java
@@ -2,15 +2,27 @@ package edu.pitt.cs.cs1635.amp224.closetcase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button findOutfit;
+    Button buildOutfit;
+    Button manageCloset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findOutfit = findViewById(R.id.button);
+        buildOutfit = findViewById(R.id.button2);
+        manageCloset = findViewById(R.id.button3);
+
+
+
+
     }
 }
 
-// Hello World!
-// Hello
+//The home page
