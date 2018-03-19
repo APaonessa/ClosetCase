c9bc3d5bb423a3715d047a2adae79e042c082091
APaonessa <amp224@pitt.edu>
2018-03-19 19:59:22 -0400

    Add more buttons.

--- app/src/main/java/edu/pitt/cs/cs1635/amp224/closetcase/OutfitScreen.java
+++ app/src/main/java/edu/pitt/cs/cs1635/amp224/closetcase/OutfitScreen.java
@@ -3,13 +3,19 @@ package edu.pitt.cs.cs1635.amp224.closetcase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class OutfitScreen extends AppCompatActivity {
 
-    Button topLeft;
-    Button topRight;
-    Button bottomLeft;
-    Button bottomRight;
+    ImageButton topLeft;
+    ImageButton topRight;
+    ImageButton bottomLeft;
+    ImageButton bottomRight;
+    ImageButton up;
+    ImageButton complete;
+    ImageView topImage;
+    ImageView bottomImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,10 @@ public class OutfitScreen extends AppCompatActivity {
         topRight = findViewById(R.id.topRightArrow);
         bottomLeft = findViewById(R.id.bottomLeftArrow);
         bottomRight = findViewById(R.id.bottomRightArrow);
+        up = findViewById(R.id.UpButton);
+        complete = findViewById(R.id.CompleteButton);
+        topImage = findViewById(R.id.imageViewTop);
+        bottomImage = findViewById(R.id.imageViewBottom);
 
     }
 }
