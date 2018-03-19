39ea8221d75f5df6919272ed41ccc7935920f214
APaonessa <amp224@pitt.edu>
2018-03-19 15:16:29 -0400

    Add outfit Screen buttons (Java)

--- app/src/main/java/edu/pitt/cs/cs1635/amp224/closetcase/OutfitScreen.java
+++ app/src/main/java/edu/pitt/cs/cs1635/amp224/closetcase/OutfitScreen.java
@@ -2,12 +2,24 @@ package edu.pitt.cs.cs1635.amp224.closetcase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class OutfitScreen extends AppCompatActivity {
+
+    Button topLeft;
+    Button topRight;
+    Button bottomLeft;
+    Button bottomRight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_outfit_screen);
+
+        topLeft = findViewById(R.id.topLeftArrow);
+        topRight = findViewById(R.id.topRightArrow);
+        bottomLeft = findViewById(R.id.bottomLeftArrow);
+        bottomRight = findViewById(R.id.bottomRightArrow);
+
     }
 }
