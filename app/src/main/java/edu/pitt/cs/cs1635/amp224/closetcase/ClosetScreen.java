dc29735c6f4dd01a00b536a01d5e96dd53bd84c5
APaonessa <amp224@pitt.edu>
2018-03-23 12:02:08 -0400

    Update Closet Screen

--- app/src/main/java/edu/pitt/cs/cs1635/amp224/closetcase/ClosetScreen.java
+++ app/src/main/java/edu/pitt/cs/cs1635/amp224/closetcase/ClosetScreen.java
@@ -2,12 +2,26 @@ package edu.pitt.cs.cs1635.amp224.closetcase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SearchView;
 
 public class ClosetScreen extends AppCompatActivity {
+
+    Button filter;
+    Button addNew;
+    SearchView search;
+    ImageButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_closet_screen);
+
+        filter = findViewById(R.id.Filter);
+        addNew = findViewById(R.id.AddNew);
+        search = findViewById(R.id.searchView);
+        back = findViewById(R.id.imageButton);
+
     }
 }
