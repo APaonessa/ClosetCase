98aaa260956e89601cd72185c4124d028b6d148d
lnm29 <lnm29@pitt.edu>
2018-04-09 07:38:13 -0400

    Update MainActivity.java
    adding help button implementation to master

--- app/src/main/java/edu/pitt/cs/cs1635/amp224/closetcase/MainActivity.java
+++ app/src/main/java/edu/pitt/cs/cs1635/amp224/closetcase/MainActivity.java
@@ -8,22 +8,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
-import me.toptas.fancyshowcase.FancyShowCaseView;
+import ru.dimorinny.showcasecard.ShowCaseView;
+import ru.dimorinny.showcasecard.position.ShowCasePosition;
+import ru.dimorinny.showcasecard.position.ViewPosition;
+import ru.dimorinny.showcasecard.radius.Radius;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+public class MainActivity extends AppCompatActivity{
 
     Button findOutfit;
     Button buildOutfit;
     Button manageCloset;
-    
-    Button foHelpButton;
-    Button boHelpButton;
-    Button mcHelpButton;
-    
     public final static String KEY_EXTRA_CONTACT_ID = "KEY_EXTRA_CONATACT_ID";
     private ListView listView;
     DBHelper dbHelper;
 
+    Button foHelpButton;
+    Button boHelpButton;
+    Button mcHelpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,47 +35,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findOutfit = findViewById(R.id.button);
         buildOutfit = findViewById(R.id.button2);
         manageCloset = findViewById(R.id.button3);
-        
-        foHelpButton = findViewById(R.id.button4);
-        boHelpButton = findViewById(R.id.button5);
-        mcHelpButton = findViewById(R.id.button6);
+
+        foHelpButton = findViewById(R.id.foHelpButton);
+        boHelpButton = findViewById(R.id.boHelpButton);
+        mcHelpButton = findViewById(R.id.mcHelpButton);
+
 
         dbHelper = new DBHelper(this);
-        dbHelper.getReadableDatabase();
 
+       /* final Cursor cursor = dbHelper.getAllClothes();
+        String[] columns = new String[]{
+                DBHelper.CLOTHES_COLUMN_ID,
+                DBHelper.CLOTHES_COLUMN_NAME
+        };*/
 
+       foHelpButton.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View view){
+               showTooltip1(new ViewPosition(view));
+           }
+       });
 
-    }
+        boHelpButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                showTooltip2(new ViewPosition(view));
+            }
+        });
 
-    @Override
-    public void onClick(View v){
-        switch (v.getId()){
-            case R.id.button4:
-                new FancyShowCaseView.Builder(this)
-                        .title("Find Outfit Button generates a custom outfit right away for you! For a quick option, click 'Find Outfit' button!")
-                        .focusOn(v)
-                        .build()
-                        .show();
-                break;
-            case R.id.button5:
-                new FancyShowCaseView.Builder(this)
-                        .title("Build Outfit Button lets you take control and create your own outfit and look through options of possible outfits! Want to decide on your own? Click 'Build Outfit' button!")
-                        .focusOn(v)
-                        .build()
-                        .show();
-                break;
-            case R.id.button6:
-                new FancyShowCaseView.Builder(this)
-                        .title("Manage Closet Button takes you straight to your closet where all your pieces you stored into the app are located!")
-                        .focusOn(v)
-                        .build()
-                        .show();
-                break;
-
-        }
+        mcHelpButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                showTooltip3(new ViewPosition(view));
+            }
+        });
 
     }
-    
+
+    private void showTooltip1(ShowCasePosition position){
+        new ShowCaseView.Builder(MainActivity.this)
+                .withTypedPosition(position)
+                .withTypedRadius(new Radius(186F))
+                .withContent("Find Outfit Button generates a custom outfit right away for you! For a quick option, click 'Find Outfit' button!")
+                .build()
+                .show(MainActivity.this);
+    }
+
+    private void showTooltip2(ShowCasePosition position){
+        new ShowCaseView.Builder(MainActivity.this)
+                .withTypedPosition(position)
+                .withTypedRadius(new Radius(186F))
+                .withContent("Build Outfit Button lets you take control and create your own outfit and look through options of possible outfits! Want to decide on your own? Click 'Build Outfit' button!")
+                .build()
+                .show(MainActivity.this);
+    }
+
+    private void showTooltip3(ShowCasePosition position){
+        new ShowCaseView.Builder(MainActivity.this)
+                .withTypedPosition(position)
+                .withTypedRadius(new Radius(186F))
+                .withContent("Manage Closet Button takes you straight to your closet where all your pieces you stored into the app are located!")
+                .build()
+                .show(MainActivity.this);
+    }
+
+
     //when find outfit and build outfit buttons are clicked, this method "activates"
     //and the screen changes from home to outfit screen
     public void goToOutfitScreen(View view){
@@ -90,5 +116,3 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 }
-
-//The home page
